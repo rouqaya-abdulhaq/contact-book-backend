e17879b4d7e5852c506be3297c60ca6f5e8fb519
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-27 21:50:46 +0300

    cleaned up registartion to database

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,55 +1,60 @@
-const users = [{name : "rouqaya", email : "rouqaya@gmail.com", password : "red", contacts : [
-    {
-        firstName : "rahaf",
-        lastName : "abdulhaq",
-        email : "rahaf@gmail.com",
-        phoneNumber : "765s"
-        },
-        {
-            firstName : "ghada",
-            lastName : "abdulhaq",
-            email : "ghada@gmail.com",
-            phoneNumber : "765373768"
-        }   
-]}];
-
-
 module.exports = (app,client) =>{
     app.post('/signUp', (req, res) =>{
         res.setHeader('Content-Type', 'application/json');
-        const newUser = req.body.firstName && req.body.lastName && req.body.email && req.body.password ? {
-            fisrtName : req.body.firstName,
-            lastName : req.body.lastName,
-            email : req.body.email,
-            password : req.body.password
-        } : null;
+        const newUser = assignNewUser(req.body);
         if(newUser){
-            client.query(`INSERT INTO users(user_first_name, user_last_name, email, password
-                ) VALUES('${newUser.firstName}','${newUser.lastName}','${newUser.email}','${newUser.password}')`,(err,response)=>{
-                    if(response){
-                        console.log(response);
-                        res.status(200).send(newUser);
-                    }else{
-                        console.log(err);
-                        res.status(400).send(err.detail)
-                    }
-                });
-            
+            addUserToDB(newUser,res,client)
         }else{
             res.status(400).send('unable to register');
         }
     });
-    
+
     app.post('/signIn', (req, res) =>{
         const password = req.body.password;
         const email = req.body.email;
-        client.query(`SELECT * FROM users WHERE email='${email}' AND password='${password}'`,(err,response)=>{
-            if(response){
-                res.status(200).send(response.rows[0]); 
-            }else{
-                res.status(401).send("no such user");
-            }
-            client.end();
-        });
+        getUserFromDB(email,password,res,client);
     });
+}
+
+const addUserToDB = (newUser,res,client) =>{
+    client.query(`INSERT INTO users(user_first_name, user_last_name, email, password
+        ) VALUES('${newUser.firstName}','${newUser.lastName}','${newUser.email}','${newUser.password}')`,(err,response)=>{
+            if(response){
+                getUserFromDB(newUser.email,newUser.password,res);
+            }else{
+                console.log(err);
+                res.status(400).send(err.detail)
+            }
+        });
+}
+const getUserFromDB = (email,password ,res,client) =>{
+    client.query(`SELECT * FROM users WHERE email='${email}' AND password='${password}'`,(err,response)=>{
+        if(response){
+            res.status(200).send(response.rows[0]); 
+        }else{
+            console.log(err);
+            res.status(401).send("no such user");
+        }
+        client.end();
+    });
+}
+
+const assignNewUser = (reqBody) =>{
+    if(checkNewUser(reqBody)){
+        return {
+            firstName : reqBody.firstName,
+            lastName : reqBody.lastName,
+            email : reqBody.email,
+            password : reqBody.password 
+        }
+    }
+    return null;
+}
+
+const checkNewUser = (reqBody) =>{
+    if(reqBody.firstName && reqBody.lastName && reqBody.email && reqBody.password){
+        return true;
+    }else{
+        return false;
+    }
 }
