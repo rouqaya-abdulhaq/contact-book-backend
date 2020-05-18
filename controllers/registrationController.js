199aa1b35e03bee5acd712883eab140c03aa5744
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-05-18 20:49:58 +0300

    compared hash password on sign in

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -22,14 +22,14 @@ module.exports = (app,client) =>{
     app.post('/signIn', (req, res) =>{
         const password = req.body.password;
         const email = req.body.email;
-        getUserFromDB(email,password,res,client);
+        authUser(email,password,res,client);
     });
 }
 
 const addingUserQuery = (newUser) =>{
     return `INSERT INTO users(user_first_name, user_last_name, email, password
         ) VALUES('${newUser.firstName}','${newUser.lastName}','${newUser.email}','${newUser.password}')
-        RETURNING email, password`;
+        RETURNING email`;
 }
 
 const addUserToDB = (newUser,res,client) =>{
@@ -37,21 +37,35 @@ const addUserToDB = (newUser,res,client) =>{
     client.query(query,(err,response)=>{
             if(response){
                 const returnedValue = response.rows[0];
-                getUserFromDB(returnedValue.email,returnedValue.password,res,client);
+                getUserFromDB(returnedValue.email,res,client);
             }else{
-                console.log(err);
                 res.status(400).send(err.detail)
             }
         });
 }
 
-const getUserFromDB = (email,password ,res,client) =>{
-    client.query(`SELECT user_first_name, user_id FROM users WHERE email='${email}' AND password='${password}'`,(err,response)=>{
+const getUserFromDB = (email,res,client) =>{
+    client.query(`SELECT user_first_name, user_id FROM users WHERE email='${email}'`,(err,response)=>{
         if(response){
             res.status(200).send(response.rows[0]); 
         }else{
-            console.log(err);
             res.status(401).send("no such user");
+        }
+    });
+}
+
+const authUser = (email,password,res,client) =>{
+    client.query(`SELECT password FROM users WHERE email='${email}'`,(err,response)=>{
+        if(err){
+            res.status(401).send("email not valid");
+        }else{
+            bcrypt.compare(password,response.rows[0].password,(err,isMatch)=>{
+                if(err) {
+                    console.log(err);
+                }else if (isMatch){
+                    getUserFromDB(email,res,client);
+                }
+            });
         }
     });
 }
